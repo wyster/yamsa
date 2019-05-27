449e598e989380c2d449191672e475975b65e28a
ilya <wyster@make.im>
2019-05-27 17:03:36 +0200

    fix: phpunit пытается что то загрузить

--- autoload.php
+++ autoload.php
@@ -3,5 +3,9 @@
 spl_autoload_register(static function($class) {
     $preparedPath = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $preparedPath = str_replace('App' . DIRECTORY_SEPARATOR, '', $preparedPath);
-    include __DIR__ . '/src/' . $preparedPath . '.php';
+    $fullPath = __DIR__ . '/src/' . $preparedPath . '.php';
+    if (!file_exists($fullPath)) {
+        return;
+    }
+    include $fullPath;
 });
